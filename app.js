933e03eae9dd280c8f91e35fbe7fc86ec967913c
Nichole McGrew <nicholemcgrew@gmail.com>
2022-01-10 14:54:55 -0600

    commit JavaScript_Variables

--- app.js
+++ app.js
@@ -4,7 +4,25 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Nichole"; 
+let lastName = "McGrew";
+var age = 36;
 
 // Exercise 2
+let fullName = firstName + " " + lastName; //NicholeMcGrew
+console.log(fullName);
+
+// Template strings denoted by backticks
+let TemplateFullName = `${firstName} ${lastName}`; // "Nichole McGrew"
+console.log(TemplateFullName);
 
 // Exercise 3
+let city = "Yukon"
+let pasttime = "coding"
+
+let myStory = "I am " + fullName + ". I live in " + city + ". I like " + pasttime + ".";
+console.log(myStory);
+
+let TemplateMyStory = `I am ${fullName}. I live in ${city}. I like ${pasttime}. `;
+console.log(TemplateFullName);
+console.log(TemplateMyStory);
